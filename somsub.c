7be126309f872716345eb5399c9db4e8fb336f92
Sleeper02 <nicolas.s.fernandes@unesp.br>
2023-09-25 17:24:13 -0300

    Add - mult

--- somsub.c
+++ somsub.c
@@ -22,11 +22,20 @@ int div(int n1, int n2){
     return (div);
 }
 
+int mult(int n1, int n2){
+    int mult;
+
+    mult = (n1*n2);
+    return (mult);
+}
+
 int main (){
     int n1, n2, op;
     
-    printf ("CR7 > Mbappe");
-    printf ("Qual operação será utilizada\n Soma = 1\n Subtração = 2\n Insira a sua escolha: ");
+    printf ("CR7 > Mbappe\n");
+
+    while (op!=9){
+    printf ("Qual operação será utilizada\n Soma = 1\n Subtração = 2\n Divisão = 3\n Multiplicação = 4\n Quit = 9\n Insira a sua escolha: ");
     scanf ("%d", &op);
 
     switch(op){
@@ -57,7 +66,19 @@ int main (){
         scanf ("%d", &n2);
         printf("A divisão foi %d", div(n1,n2));
         break;
-    }
 
+        case 4:
+
+        printf("Insira o primeiro numero: ");
+        scanf ("%d", &n1);
+        printf("Insira o segundo numero: ");
+        scanf ("%d", &n2);
+        printf("A multiplicação foi %d", mult(n1,n2));
+        break;
+
+        case 9:
+        break;
+    }
+    }
         return 0;
     }
