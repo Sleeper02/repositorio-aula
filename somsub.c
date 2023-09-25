2fb417dfa0cac1b399d5588750b857ad68df4f4f
henriqueFinco <rick.finco@gmail.com>
2023-09-25 17:14:33 -0300

    divisão adicionada

--- somsub.c
+++ somsub.c
@@ -15,6 +15,13 @@ int sub(int n1, int n2){
     return (sub);
 }
 
+int div(int n1, int n2){
+    int div;
+
+    div = n1/n2;
+    return (div);
+}
+
 int main (){
     int n1, n2, op;
     
@@ -40,6 +47,15 @@ int main (){
         printf("Insira o segundo numero: ");
         scanf ("%d", &n2);
         printf("A subtração foi %d", sub(n1,n2));
+        break;
+
+        case 3:
+
+        printf("Insira o primeiro numero: ");
+        scanf ("%d", &n1);
+        printf("Insira o segundo numero: ");
+        scanf ("%d", &n2);
+        printf("A divisão foi %d", div(n1,n2));
         break;
     }
 
